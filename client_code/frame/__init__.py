f98650d7461480d9df26b5c5a9b0444b79a9f97a
oaadmin@protonmail.com <oaadmin@protonmail.com>
2024-06-03 19:36:39 +0000

    Edited frame

--- client_code/frame/__init__.py
+++ client_code/frame/__init__.py
@@ -53,7 +53,7 @@ class frame(frameTemplate):
     event_filter = self.gofurs_contract.filters[event_name](*args)
     if get_open_form().wc.chainId==666666666:
       print("degen")
-      from_block = 1239792
+      from_block = 1239792 
     logs = self.gofurs_contract.queryFilter(event_filter, from_block, to_block)
     processed_logs = [log.args for log in logs]  # Replace with your own logic if necessary
         
